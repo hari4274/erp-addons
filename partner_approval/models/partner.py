f203ddd154d5670311eccc3038933392045cc606
hari prasath <bala.hariprasath@gmail.com>
2020-04-10 03:04:01 +0530

    Indentation convert to spaces

--- partner_approval/models/partner.py
+++ partner_approval/models/partner.py
@@ -6,12 +6,12 @@ from odoo.exceptions import ValidationError
 
 
 class Partner(models.Model):
-	_inherit = 'res.partner'
+    _inherit = 'res.partner'
 
-	need_approval = fields.Boolean('Need Approval')
+    need_approval = fields.Boolean('Need Approval')
 
-	def action_validate(self):
-		if self.env.user.has_group('base.group_no_one'):
-			self.write({'need_approval': False, 'active': True})
-		else:
-			raise ValidationError('You are not allowed To validate partner !!! Contact Administrator.')
+    def action_validate(self):
+        if self.env.user.has_group('base.group_no_one'):
+            self.write({'need_approval': False, 'active': True})
+        else:
+            raise ValidationError('You are not allowed To validate partner !!! Contact Administrator.')
